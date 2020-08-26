ada143f6acee6675b981ea26ec04e7f6d49af399
shotaro yamamoto <shotoron@gmail.com>
2020-08-26 16:17:01 +0900

    [add]ログイン/ログアウト機能を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def new 
+    @user = User.new 
+  end  
+
+  def create 
+    @user = User.new(user_params) 
+    if @user.save 
+      redirect_to new_session_path 
+    else 
+      render :new 
+    end 
+  end
+
+  private
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation) 
+  end  
+
+  # def find_user 
+  #   @user = User.find(params[:id]) 
+  # end
 end
