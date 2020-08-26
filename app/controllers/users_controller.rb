b6aa490f684e5b922cbd292c95d12b7086df2de0
shotaro yamamoto <shotoron@gmail.com>
2020-08-26 17:35:00 +0900

    [?]お手上げ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,15 @@
 class UsersController < ApplicationController
-
   def new 
     @user = User.new 
-  end  
+  end
 
-  def create 
-    @user = User.new(user_params) 
-    if @user.save 
-      redirect_to new_session_path 
-    else 
-      render :new 
-    end 
+  def create
+    user = params[:user].permit(:name, :email, :password, :password_digest, :password_confirmation)
+    User.create(user)
   end
 
   private
-  def user_params 
-    params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation) 
-  end  
-
-  # def find_user 
-  #   @user = User.find(params[:id]) 
+  # def user_params
+  #   params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation)
   # end
 end
